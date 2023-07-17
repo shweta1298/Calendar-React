63396bfc051480b917cdb5a34c5e20d2baa38128
Shweta <shwetan1204@gmail.com>
2023-07-17 18:46:41 +0530

    small calendar css changes

--- src/components/calendar/components/Event.js
+++ src/components/calendar/components/Event.js
@@ -3,7 +3,7 @@ import React from 'react'
 function EventBar(props) {
     const { event } = props;
     return (
-        <div key={event.id} className="event-bar-wrapper">
+        <div key={event.id} className="event-bar-wrapper" >
             <div className='event-bar'>{event.title}</div>
         </div>
     )
--- src/components/calendar/components/Sidebar.js
+++ src/components/calendar/components/Sidebar.js
@@ -10,37 +10,40 @@ function Sidebar(props) {
 
     return (
         <div className={isOpen ? "sidebar-wrapper-open" : "sidebar-wrapper"}>
-            {/* <div className="create-task-button-wrapper">
+            <div className="create-task-button-wrapper">
                 <button className='create-task-button'>
                     <FontAwesomeIcon
                         icon={faSquarePlus}
                     // title="Next Month"
                     //  className='create-task-button'
+                    size='lg'
                     >
                     </FontAwesomeIcon>
                     <span className='create-task-title'>Create Task</span>
 
                 </button>
 
-            </div> */}
+            </div>
             <div className="small-calendar-wrapper">
-                <div className="small-calendarheader">
-                    <div className="small-calendar-monthname">{getMonthName(monthIndex, year)}</div>
+                {/* <div className="small-calendarheader"> */}
                     <div className="navigate-wrapper">
+                        <div className="small-calendar-monthname">{getMonthName(monthIndex, year)}</div>
+
                         <div className="up-button">
                             <FontAwesomeIcon icon={faChevronUp} size='sm' />
                         </div>
                         <div className="down-button">
-                        <FontAwesomeIcon icon={faChevronDown} size='sm' />
+                            <FontAwesomeIcon icon={faChevronDown} size='sm' />
 
                         </div>
                     </div>
+                {/* </div> */}
+                <div className="small-calendar">
+                    {currentMonth.map((week, weekIndex) => week.map((day) => {return isCurrentMonth(day.month(), monthIndex) && <div key={day.day()} className='small-calendar-day' >{day.date()}</div>}))}
+
                 </div>
             </div>
-            <div className="small-calendar">
-                {currentMonth.map((week, weekIndex) => week.map((day) => <div key={day.day()} >{isCurrentMonth(day.month(), monthIndex) && day.date()}</div>))}
 
-            </div>
         </div>
     )
 }
